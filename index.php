1cabe4653be8c63347d801c78de14842a1ea9c1b
fathun <fat.nida75@gmail.com>
2019-09-25 13:54:37 +0700

    commit 1

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
+    <div class="container">
+  <a class="navbar-brand" href="#">Polinema</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Galery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact US</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+  <img src="psdku.png" width="300">
+    <h1 class="display-4">PSDKU Kota Kediri</h1>
+    <p class="lead">Selamat Datang di Polinema PSDKU Kota Kediri</p>
+  </div>
+</div>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-sm">
+      <h1 class="bold">About</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      The ordinary life of 17-year-old otaku Hajime Nagumo is disrupted when he and his classmates are summoned to a fantasy world and tasked with saving mankind. While his classmates are gifted with impressive abilities useful in combat, Hajime is belittled for only gaining an inferior transmutation skill that lacks any real offensive power. During an expedition in the Great Orcus Labyrinth, Hajime is betrayed by one of his classmates, plummeting him to the bottom of an abyss.
+    </div>
+    <div class="col-sm">
+       Though he survives the fall, Hajime is faced with menacing monsters and misfortunes that send him spiraling into a grim nightmare. Desperate to live and return home one day, he resolves to fight for his survival—only to meet an imprisoned vampire he names Yue, who is also seeking to escape the labyrinth. Taking an interest in him, Yue and a few others along the way accompany Hajime on his journey to find a way back home, while steadily transforming from commonplace to the world's strongest.
+    </div>
+  </div>
+</div>
+<div class="container mt-5">
+  <div class="row text-center">
+    <div class="col-sm">
+      <h1 class="bold">Gallery</h1>
+    </div>
+  </div>
+  <div class="row">
+      <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+  <img src="1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">This is garbage and I say this as a person that likes the isekai genre.
+    The thing is, they kinda forgot about the "isekai" part.</p>
+    <a href="#" class="btn btn-primary">Terjemahkan</a>
+  </div>
+</div>
+</div>
+<div class="col-sm">
+<div class="card" style="width: 18rem;">
+  <img src="2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">There's no transportation to another world in this anime because for some reason they skipped it. The pacing is terrible, instead of things playing out as they should they put everything into flashbacks, confusing everyone.</p>
+    <a href="#" class="btn btn-primary">Terjemahkan</a>
+  </div>
+</div>
+</div>
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+  <img src="3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">And honestly that might have been their goal considering that if you haven't read the source material, nothing in this adaptation is going to make sense.</p>
+    <a href="#" class="btn btn-primary">Terjemahkan</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<div class="container mt-5">
+  <div class="row text-center">
+    <div class="col-sm">
+      <h1 class="bold">Contact US</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Alamat</h5>
+    <br class="card-text">JL     : Cendrawasih Rt/Rw : 3/2</br>
+    <br class="card-text">Kec    : Ringinrejo</br>
+    <br class="card-text">Kab    : Kediri</br>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Input Name">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Email</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Input Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Pesan</label>
+    <textarea type="password" class="form-control" id="exampleInputPassword1" placeholder="Input Pesan"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+  </div>
+</div>
+<footer class="bg-info text-white mt-4 pt-3">
+    <div class="container">
+    <div calss="row">
+    <div class="col text-center">
+    <p>Copyrige 2019</p>
+    </div>
+    </div>
+    </div>
+</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
